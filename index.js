19455451f084b4c89f9bead5f8b7b7981482a59f
Andres Olarte <78939028+AndresOlarteZabala@users.noreply.github.com>
2023-04-29 15:00:10 -0500

    Fixed error associated with get module by default

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-
 const app = express();
 const url = 'mongodb://localhost:27017/mydb';
 
+// Set up routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
+
   console.log('Connected to MongoDB!');
   
-  // Set up routes
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-
   // Insert a new user document
   app.post('/users', (req, res) => {
     const user = { name: req.body.name, email: req.body.email, created_date: new Date() };
@@ -32,7 +32,6 @@ MongoClient.connect(url, function(err, db) {
       res.send(result);
     });
   });
-
 });
 
 app.listen(3000, function() {
